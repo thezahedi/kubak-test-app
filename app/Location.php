433bcb082c51c880ed3aca433c06f4aa1d3d31a5
Saman Zahedi <isamanzahedi@gmail.com>
2019-09-04 09:18:35 +0000

    Apply fixes from StyleCI

--- app/Location.php
+++ app/Location.php
@@ -15,7 +15,7 @@ class Location extends Model
     public static function getLocations($lat, $lng, $limit)
     {
         $get = DB::select("SELECT *, ((ACOS(SIN($lat * PI() / 180) * SIN(lat * PI() / 180) + COS($lat * PI() / 180) * COS(lat * PI() / 180) * COS(($lng - lng) * PI() / 180)) * 180 / PI()) * 60 * 1.1515 * 1.609344) as distance FROM locations WHERE lat != '0.0' AND lng != '0.0' ORDER BY distance ASC LIMIT $limit");
+
         return $get;
     }
-
 }
